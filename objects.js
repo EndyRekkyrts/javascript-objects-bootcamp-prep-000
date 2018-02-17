18cde8c82e6af3a9af27aa3e2b44332c6bcae99e
Carrollyn Catanzaro <endyrekkyrts@yahoo.com>
2018-02-17 14:34:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {"Phil Ochs": "Here's to the State of Mississippi", "Kanye:" "Gold Digger"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artis] = [song]
+  playlist[artis] = song
   return playlist
 }
 
