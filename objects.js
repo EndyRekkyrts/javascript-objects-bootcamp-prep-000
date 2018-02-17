c90f1fd145e050f6ba7dcdad9200a58af56435c8
Carrollyn Catanzaro <endyrekkyrts@yahoo.com>
2018-02-17 14:34:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {Phil Ochs: "Here's to the State of Mississippi", Kanye: "Gold Digger"}
+var playlist = {"Phil Ochs": "Here's to the State of Mississippi", Kanye: "Gold Digger"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artis] = song
