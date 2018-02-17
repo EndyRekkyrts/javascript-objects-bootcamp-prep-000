d56f49b2eed462f1121fe17c13e4637015c0f2e5
Carrollyn Catanzaro <endyrekkyrts@yahoo.com>
2018-02-17 14:30:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artistName: 'songTitle'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({}, playlist, {artistName: 'songTitle'})
+  Object.assign({}, playlist, {"Phil Ochs": "Here's to the State of Mississippi"})
   return playlist
 }
 
